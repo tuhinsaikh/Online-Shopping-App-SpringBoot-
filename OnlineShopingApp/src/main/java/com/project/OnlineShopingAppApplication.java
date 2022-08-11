ee67fbd0d4da1716c4a001d5fc456321891fe34d
Tuhin Saikh <101566519+tuhinsaikh@users.noreply.github.com>
2022-08-11 12:53:50 +0530

    Add files via upload

---
+++ OnlineShopingApp/src/main/java/com/project/OnlineShopingAppApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopingAppApplication.class, args);
+	}
+
+}

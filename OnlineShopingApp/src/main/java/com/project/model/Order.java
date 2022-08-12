874d915d4cc47568afca2608ecdc666200128352
Tuhin Saikh <101566519+tuhinsaikh@users.noreply.github.com>
2022-08-12 12:04:43 +0530

    Update Order.java

--- OnlineShopingApp/src/main/java/com/project/model/Order.java
+++ OnlineShopingApp/src/main/java/com/project/model/Order.java
@@ -9,8 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
-import com.masai.model.Book;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;

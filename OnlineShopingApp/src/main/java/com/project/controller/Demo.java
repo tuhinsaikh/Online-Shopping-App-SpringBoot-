e72e54a06fdfffad913187ffd43a187f78d3487f
Sam01-dev <sampanna0029@gmail.com>
2022-08-12 11:59:34 +0530

    address added

--- OnlineShopingApp/src/main/java/com/project/controller/Demo.java
+++
@@ -1,5 +0,0 @@
-package com.project.controller;
-
-public class Demo {
-
-}
---
+++ OnlineShopingApp/src/main/java/com/project/model/Address.java
@@ -0,0 +1,35 @@
+package com.project.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer addressId;
+	private String streetNo;
+	private String buildingName;
+	private String city;
+	private String state;
+	private String country;
+	private String pincode;
+	
+	
+	
+	
+
+}

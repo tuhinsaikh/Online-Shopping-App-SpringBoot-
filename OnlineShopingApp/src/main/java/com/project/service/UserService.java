5a9599371b192d0a1c58b25f12f2b9f9976ae566
Tuhin Saikh <101566519+tuhinsaikh@users.noreply.github.com>
2022-08-13 19:25:03 +0530

    update credential, log out added

--- OnlineShopingApp/src/main/java/com/project/service/UserService.java
+++ OnlineShopingApp/src/main/java/com/project/service/UserService.java
@@ -3,5 +3,10 @@ package com.project.service;
 import com.project.model.User;
 
 public interface UserService {
+	
 	public User saveUer(User user);
+	
+	public User updateUserCredential(User user,String key);
+	
+	public String userLogout(String key);
 }
--- OnlineShopingApp/src/main/java/com/project/service/UserServiceImpl.java
+++ OnlineShopingApp/src/main/java/com/project/service/UserServiceImpl.java
@@ -2,12 +2,16 @@ package com.project.service;
 
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 import com.project.exception.UserAlreadyExists;
+import com.project.model.CurrentUserSession;
 import com.project.model.User;
+import com.project.repository.CurrentUserSessionDao;
 import com.project.repository.UserDao;
 
 @Service
@@ -15,6 +19,9 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private UserDao uDao;
+	
+	@Autowired
+	private CurrentUserSessionDao cuserDao;
 
 	@Override
 	public User saveUer(User user) {		
@@ -22,4 +29,29 @@ public class UserServiceImpl implements UserService{
 		if(u==null) return uDao.save(user);
 		else throw new UserAlreadyExists("User Already Exists");
 	}
+
+	@Override
+	public User updateUserCredential(User user, String key) {
+		CurrentUserSession cuser =  cuserDao.findByUniqueId(key);
+		if(cuser==null) throw new UserAlreadyExists("user not loged in");
+		Optional<User> opt = uDao.findById(cuser.getUserId());
+		if(opt.isEmpty()) throw new UserAlreadyExists("user not found");
+		User userWithSameNumber = uDao.findByMobile(user.getMobile());
+		if(userWithSameNumber!=null) throw new UserAlreadyExists("one user find with the same mobile number");
+		User saveUser = uDao.save(user);
+		cuserDao.delete(cuser);
+		uDao.delete(opt.get());
+		return saveUser;
+	}
+
+	@Override
+	public String userLogout(String key) {
+		CurrentUserSession cuser =  cuserDao.findByUniqueId(key);
+		if(cuser==null) throw new UserAlreadyExists("user not loged in");
+		cuserDao.delete(cuser);
+		return "Logged Out successfully";
+	}
+
+
+	
 }

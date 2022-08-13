0c5345e2058f2bf5df32676de4fb2a836c7c09eb
Sampanna Chatterjee <83101767+Sam01-dev@users.noreply.github.com>
2022-08-13 12:36:16 +0530

    Add files via upload

--- OnlineShopingApp/src/main/java/com/project/repository/AddressDao.java
+++ OnlineShopingApp/src/main/java/com/project/repository/AddressDao.java
@@ -1,11 +1,18 @@
 package com.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.project.model.Address;
 
+
 @Repository
 public interface AddressDao extends JpaRepository<Address, Integer>{
+	
+	public Address findByaddressId(Integer addressId);
+	
+	public List<Address> findAll();
 
 }

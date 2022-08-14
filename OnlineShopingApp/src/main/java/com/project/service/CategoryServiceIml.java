a518de1f58aa6d18f4a8033e84c631b1daec468b
Tuhin Saikh <101566519+tuhinsaikh@users.noreply.github.com>
2022-08-14 21:55:58 +0530

    Add files via upload

--- OnlineShopingApp/src/main/java/com/project/service/CategoryServiceIml.java
+++ OnlineShopingApp/src/main/java/com/project/service/CategoryServiceIml.java
@@ -1,18 +1,23 @@
 package com.project.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.project.model.Category;
+import com.project.repository.CategoryDao;
+import com.project.repository.ProductDao;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.model.Category;
-//import com.project.model.Course;
 import com.project.model.Product;
 import com.project.repository.CategoryDao;
 import com.project.repository.ProductDao;
 
 @Service
-public class ProductServiceImpl implements ProductService{
+public class CategoryServiceIml implements CategoryService{
 
 	
 	@Autowired
@@ -21,10 +26,9 @@ public class ProductServiceImpl implements ProductService{
 	@Autowired
 	private CategoryDao cDao;
 	
-	
 	@Override
-	public Product saveProduct(Product product) {
-		
+	public Category addCategory(Category category) {
+		// TODO Auto-generated method stub
 //		List<Category> categories= product.getCategories();
 //		
 //		for(Category cat:categories) {
@@ -34,9 +38,22 @@ public class ProductServiceImpl implements ProductService{
 //		}
 //		
 //		return pDao.save(product);
-		return null;
+		
+	List<Product>	products=category.getProducts();
+	
+		if(products.size()!=0) {
+			for(Product prd: products) {
+				
+				prd.setCategory(category);
+				//pDao.save(prd);
+				
+			}
+		}
+			
+	
+	return cDao.save(category);
+	 
+		//return null;
 	}
 
-	
-		
 }

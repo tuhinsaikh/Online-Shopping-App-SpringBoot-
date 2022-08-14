bf2b7358c7b727e33c7a4320685eba526f14fb3b
Tuhin Saikh <101566519+tuhinsaikh@users.noreply.github.com>
2022-08-14 21:53:47 +0530

    Add files via upload

--- OnlineShopingApp/src/main/java/com/project/repository/ProductDao.java
+++ OnlineShopingApp/src/main/java/com/project/repository/ProductDao.java
@@ -1,5 +1,7 @@
 package com.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.project.model.Product;
 @Repository
 public interface ProductDao extends JpaRepository<Product, Integer>{
 
+	List<Product> findByProductName(String productName);
 }

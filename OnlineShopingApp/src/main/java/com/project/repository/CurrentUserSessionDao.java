147aa1d3ee223c02b30f5204c16d517ecb488ec6
AnniRoy <101575131+AnniRoy@users.noreply.github.com>
2022-08-14 17:42:20 +0530

    mapped with user

--- OnlineShopingApp/src/main/java/com/project/repository/CurrentUserSessionDao.java
+++ OnlineShopingApp/src/main/java/com/project/repository/CurrentUserSessionDao.java
@@ -1,8 +1,10 @@
 package com.project.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.project.model.CurrentUserSession;
 
@@ -12,6 +14,9 @@ public interface CurrentUserSessionDao extends JpaRepository<CurrentUserSession,
    public Optional<CurrentUserSession> findById(Integer userId);
    
    public CurrentUserSession  findByUniqueId(String uniqueId);
+   
+   @Query("select c.Id from CurrentUserSession c where c.UserId=?1")
+   public String findByUserId(Integer userId);  
    
    
 }

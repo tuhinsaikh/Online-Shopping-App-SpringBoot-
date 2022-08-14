d1e3dc1d0242073dc0c891df60642db5a0021d1a
Tuhin Saikh <101566519+tuhinsaikh@users.noreply.github.com>
2022-08-14 22:00:44 +0530

    Add files via upload

---
+++ OnlineShopingApp/src/main/java/com/project/repository/OrderService.java
@@ -0,0 +1,8 @@
+package com.project.service;
+
+import com.project.model.MyOrder;
+
+public interface OrderService {
+
+	public MyOrder addOrder(MyOrder order,Integer customerId,Integer addressId);
+}

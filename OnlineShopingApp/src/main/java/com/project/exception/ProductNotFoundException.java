cb4d9bd6cf7a82e9aaf3bd5ae90269dbc4ad5b00
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-08-15 23:31:26 +0530

    Add files via upload

---
+++ OnlineShopingApp/src/main/java/com/project/exception/ProductNotFoundException.java
@@ -0,0 +1,13 @@
+package com.project.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+	public ProductNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ProductNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+		
+		super(message);
+	}
+}

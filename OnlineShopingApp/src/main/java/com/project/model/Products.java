b20b46e9075f32a20d65176508d3fd79e8949181
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-08-15 23:32:30 +0530

    Add files via upload

--- OnlineShopingApp/src/main/java/com/project/model/Products.java
+++ OnlineShopingApp/src/main/java/com/project/model/Products.java
@@ -7,6 +7,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Products {
 		
@@ -15,62 +22,15 @@ public class Products {
 	private Integer productId;
 	private String productName;
 	private Double price;
-//	private String color; 
-//	private String dimension;
-//	private String specification;
-//	private String manufacturer;
-//	private Integer quantity;
+	private String color; 
+	private String dimension;
+	private String specification;
+	private String manufacturer;
+	private Integer quantity;
 	
 	@Enumerated(EnumType.STRING)
 	private CategoryEnum category;
 
-	@Override
-	public String toString() {
-		return "Products [productId=" + productId + ", productName=" + productName + ", price=" + price + ", category="
-				+ category + "]";
-	}
 
-	public Products(Integer productId, String productName, Double price, CategoryEnum category) {
-	super();
-	this.productId = productId;
-	this.productName = productName;
-	this.price = price;
-	this.category = category;
-}
-
-	public Products() {
-		// TODO Auto-generated constructor stub
-	}
 	
-	public Integer getProductId() {
-		return productId;
-	}
-
-	public void setProductId(Integer productId) {
-		this.productId = productId;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public CategoryEnum getCategory() {
-		return category;
-	}
-
-	public void setCategory(CategoryEnum category) {
-		this.category = category;
-	}
 }

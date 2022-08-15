45b7df37f976f137264d55d36b3b038c07f304db
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-08-15 23:33:07 +0530

    Add files via upload

--- OnlineShopingApp/src/main/java/com/project/repository/ProductsDao.java
+++ OnlineShopingApp/src/main/java/com/project/repository/ProductsDao.java
@@ -10,18 +10,18 @@ import org.springframework.stereotype.Repository;
 
 import com.project.model.CategoryEnum;
 import com.project.model.Products;
+import com.project.model.ProductsDTO;
+
 
 @Repository
 public interface ProductsDao extends JpaRepository<Products, Integer> {
 	
-//	@Query("select p.productName,p.price from Products p where p.category=:catenum")
-//	public List<Products> getAllProductsInACategory(@Param("catenum") CategoryEnum catenum);
-	
 	public List<Products>  findByProductName(String productName);
 	
+//	@Query("select new com.project.model.ProductsDTO(p.productName, p.price) from Products p where p.productId=?1")
+//	public Products getproductsById(int id);
 	
-//	@Query("from Products p.category=?1")
-//	public List<Products> getAllProductsByCategory(@Param("category") String category);
-		
-   
+	@Query("select new com.project.model.ProductsDTO(p.productName, p.price) from Products p where p.category=?1")
+	public List<ProductsDTO> getCategoryWiseProducts(CategoryEnum cat);
+
 }

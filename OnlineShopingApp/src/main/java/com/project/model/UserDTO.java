837a8d2ea863892ced5710cb28c08b2c5f98c52a
Tuhin Saikh <101566519+tuhinsaikh@users.noreply.github.com>
2022-08-16 23:37:29 +0530

    Add files via upload

--- OnlineShopingApp/src/main/java/com/project/model/UserDTO.java
+++ OnlineShopingApp/src/main/java/com/project/model/UserDTO.java
@@ -1,6 +1,7 @@
 package com.project.model;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +15,7 @@ import lombok.ToString;
 public class UserDTO {
 	
 	@NotNull(message = "Enter Your Mobile Number")
+	@Pattern(regexp = "[7896]{1}[0-9]{9}",message = "Input a valid mobile number")
 	private String mobileNo;
 	@NotNull(message = "Enter Your Password")
 	private String password;

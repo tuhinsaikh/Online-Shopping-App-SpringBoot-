330426f7962b58cec2b3cbb261d2424376f9895b
Shubham Mishra <mishrashubham4606@gmail.com>
2022-08-16 17:29:55 +0530

    ALL method added

--- OnlineShopingApp/src/main/java/com/project/service/CartServiceImpl.java
+++ OnlineShopingApp/src/main/java/com/project/service/CartServiceImpl.java
@@ -3,23 +3,36 @@ package com.project.service;
 import java.util.List;
 import java.util.Optional;
 
+//import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.project.exception.CustomerNotFoundException;
 import com.project.exception.ProductNotFoundException;
 import com.project.model.Cart;
 import com.project.model.Customer;
 import com.project.model.Products;
+import com.project.model.User;
 import com.project.repository.AddressDao;
 import com.project.repository.CartDao;
+import com.project.repository.CurrentUserSessionDao;
 import com.project.repository.CustomerDao;
 import com.project.repository.ProductsDao;
+import com.project.repository.UserDao;
 
 @Service
 public class CartServiceImpl implements CartService{
 	
 	@Autowired
 	private ProductsDao pDao;
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+	private CurrentUserSessionDao currentUserSessionDao;
 	
 	@Autowired
 	private CartDao cartDao;
@@ -30,45 +43,41 @@ public class CartServiceImpl implements CartService{
 	@Autowired
 	private AddressDao addressDao;
 
-//	@Override
-//	public Cart addProductToCart(Cart cart, String name, Integer quantity) {
-		// TODO Auto-generated method stub
-		
-//	List<Product> p1=pDao.findByProductName(name);
-	
-//	List<Products> prod=cart.getCartItem();
-//	for(Products p:prod) {
-//		
-//		List<Products> pr = pDao.findByProductName(p.getProductName());
-////		List<Products> pr1 = pDao.findByProductId(p.getProductId());
-//		if(pr.size()<=0 ) {
-//			throw new ProductNotFoundException("Product not found");
-//		}
-//		if(pr1.size()<=0 ) {
-//			throw new ProductNotFoundException("Product not found");
-//		}
-//	}
-	
-//		pDao.findById(quantity);
-//		pDao.findBy
-		
-//		return null;
-//	}
 	
 	//Method to add the Product and customer in cart
 
 	@Override
-	public Cart AddProduct(Cart cart,Integer id,Integer custId) {
-		// TODO Auto-generated method stub
-		Optional<Products> opt=pDao.findById(id);
-		Optional<Customer> customer=custDao.findById(custId);
+	public Cart AddProduct(Cart cart,Integer Productid,Integer customerId) {
+		// TODO Auto-generated method stub\
+		
+		Optional<Products> opt=pDao.findById(Productid);
+		Optional<Customer> customer=custDao.findById(customerId);
 //		System.out.println(customer.get());
+		
+		
+		
+		
+//		Optional< Customer> databaseCustomer = custDao.findById(customerId);
+		
+		if(customer.isEmpty()) {
+			throw new CustomerNotFoundException("customer not found");
+		}
+//		Customer getCustomer = databaseCustomer.get();
+		User user=  userDao.findByMobile(customer.get().getMobileNumber());
+		String logedinOrNot = currentUserSessionDao.findByUserId(user.getUserId());
+		if(logedinOrNot==null) {
+			throw new CustomerNotFoundException("Customer not logged in");
+		}
+		
 		
 		
 		if(opt.isPresent()) {
 		
 			Products prod=opt.get();
 			Customer cust=customer.get();
+			
+			
+			
 			cart.setCartItem(prod);
 			cart.setCustomerlist(cust);
 //			for(Customer cust:customer) {				
@@ -90,23 +99,52 @@ public class CartServiceImpl implements CartService{
 		
 		return list;
 	}
-
 	
+	
+	//Method to update the Product from cart
 //	@Override
-//	public Cart addProductToCart(String name, int quantity) {
+//	public Cart UpdateCartProduct(Cart cart) throws ProductNotFoundException {
 //		// TODO Auto-generated method stub
-//		Cart cart=cartDao.getById(null);
 //		
-//		List<Product> p1=pDao.findByProductName(name);
+//		Optional<Cart> opt = cartDao.findById(cart.getCartItemId());
+//
+//		if (opt.isPresent()) {
+//			opt.get();
+//			Cart crt = cartDao.save(cart);
+//			return crt;
+//		} else
+//			throw new ProductNotFoundException("Product not found with given id");
 //		
-//		if(p1.size()!=0) {
-//			for(Product product: p1) {
-////				cartDao.get;
-//			}
-//		}
-//		
-//		return null;
 //	}
+//	
+
+	//Method to delete the product from cart
+	
+	@Override
+    
+	public String deleteProductfromCart(Integer id)throws ProductNotFoundException {
+		Optional<Cart> opt = cartDao.findById(id);
+		
+		if (opt.isPresent()) {
+			Cart cart = opt.get();
+//			System.out.println(prod);
+			cartDao.delete(cart);
+			return "CartProduct is deleted from Cart";
+			
+		} else
+			throw new ProductNotFoundException("Product not found with given id");
+		
+		
+	}
+
+	private Cart cart;
+	@Override
+	@Transactional
+	public void deleteAllCart() {
+		// TODO Auto-generated method stub
+		cartDao.DeleteAll();
+//		return "Cart is empty";
+	}
 
 	
 
